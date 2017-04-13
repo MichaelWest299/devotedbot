4053ef892266c9b3d17547abca9da9e24a8144f5
Michael West <michaelwest299@gmail.com>
2017-04-13 16:54:21 +0100

    deleted test file

--- testsecrets.py
+++
@@ -1,4 +0,0 @@
-CLIENT_ID = 289291325780525057
-CLIENT_SECRET = "S3vcUIFhl1ZVMTpHhZnqJovD-Isct6Yh"
-BOT_TOKEN = "Mjg5MjkxMzI1NzgwNTI1MDU3.C8AFKg.Ro-5csWwyGUCQFhr3fKzzdq0V6Q"
-CREDS = '{"user":{"email":"cubicuboctahedron@gmail.com","password":"123456"}}'
