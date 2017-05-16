a3844fb22865b46078aa7be1e9fbcdec09d58b3f
Michael West <michaelwest299@gmail.com>
2017-05-16 16:58:32 +0100

    enabled any subreddit chosen by user

--- cogs/reddit.py
+++ cogs/reddit.py
@@ -2,6 +2,7 @@ from discord.ext import commands
 import discord
 import praw
 import random
+from prawcore import NotFound
 
 
 class Reddit:
@@ -9,30 +10,29 @@ class Reddit:
     def __init__(self, bot):
         self.bot = bot
 
+    def sub_exists(self, sub):
+        exists = True
+        try:
+            reddit = praw.Reddit(client_id=self.bot.PRAW_CLIENT_ID,
+                                 client_secret=self.bot.PRAW_CLIENT_SECRET,
+                                 user_agent=self.bot.PRAW_USER_AGENT)
+            reddit.subreddits.search_by_name(sub, exact=True)
+        except NotFound:
+            exists = False
+        return exists
+
     @commands.command()
-    async def aww(self):
-        list_aww = []
-        reddit = praw.Reddit(client_id=self.bot.PRAW_CLIENT_ID,
-                             client_secret=self.bot.PRAW_CLIENT_SECRET,
-                             user_agent=self.bot.PRAW_USER_AGENT)
-
-        for submission in reddit.subreddit('aww').hot(limit=128):
-            list_aww.append(submission.url)
-
-        await self.bot.say(random.choice(list_aww))
-
-    @commands.command()
-    async def meme(self):
-        list_dankmemes = []
-        reddit = praw.Reddit(client_id=self.bot.PRAW_CLIENT_ID,
-                             client_secret=self.bot.PRAW_CLIENT_SECRET,
-                             user_agent=self.bot.PRAW_USER_AGENT)
-
-        for submission in reddit.subreddit('memes').hot(limit=128):
-            list_dankmemes.append(submission.url)
-
-        await self.bot.say(random.choice(list_dankmemes))
-
+    async def r(self, sub: str):
+        if self.sub_exists(sub):
+            content_list = []
+            reddit = praw.Reddit(client_id=self.bot.PRAW_CLIENT_ID,
+                                 client_secret=self.bot.PRAW_CLIENT_SECRET,
+                                 user_agent=self.bot.PRAW_USER_AGENT)
+            for submission in reddit.subreddit(sub).hot(limit=128):
+                content_list.append(submission.url)
+            await self.bot.say(random.choice(content_list))
+        else:
+            await self.bot.say('Sorry that subreddit does not exist')
 
 def setup(bot):
     bot.add_cog(Reddit(bot))
