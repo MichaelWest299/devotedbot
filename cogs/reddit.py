672dfe4253c8ed07f5531fcf087c1e1f1a25fc96
Michael West <michaelwest299@gmail.com>
2017-05-30 15:35:36 +0100

    restrict reddit spam to random channel

--- cogs/reddit.py
+++ cogs/reddit.py
@@ -21,18 +21,25 @@ class Reddit:
             exists = False
         return exists
 
-    @commands.command()
-    async def r(self, sub: str):
-        if self.sub_exists(sub):
-            content_list = []
-            reddit = praw.Reddit(client_id=self.bot.PRAW_CLIENT_ID,
-                                 client_secret=self.bot.PRAW_CLIENT_SECRET,
-                                 user_agent=self.bot.PRAW_USER_AGENT)
-            for submission in reddit.subreddit(sub).hot(limit=128):
-                content_list.append(submission.url)
-            await self.bot.say(random.choice(content_list))
+    @commands.command(pass_context=True)
+    async def r(self, ctx, sub: str):
+        if(ctx.message.channel == self.bot.get_channel(self.bot.RANDOM_ROOM_ID)):
+            if self.sub_exists(sub):
+                content_list = []
+                reddit = praw.Reddit(client_id=self.bot.PRAW_CLIENT_ID,
+                                     client_secret=self.bot.PRAW_CLIENT_SECRET,
+                                     user_agent=self.bot.PRAW_USER_AGENT)
+                for submission in reddit.subreddit(sub).hot(limit=128):
+                    content_list.append(submission.url)
+                await self.bot.say(random.choice(content_list))
+            else:
+                await self.bot.say('Sorry, that subreddit does not exist.')
         else:
-            await self.bot.say('Sorry that subreddit does not exist')
+            await self.bot.say("Please use the random channel.")
+
+
+
+
 
 def setup(bot):
     bot.add_cog(Reddit(bot))
