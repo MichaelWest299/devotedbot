d06d1a68552a18b32cbb9cfe0d5744cb58d65ddd
Michael West <michaelwest299@gmail.com>
2017-06-01 23:42:21 +0100

    added custom twitch alert channel

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 from discord.ext import commands
 import discord
+from cogs.utils import checks
 import json, asyncio
 
 try:
@@ -9,7 +10,7 @@ except ImportError:
 else:
     asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 
-initial_extensions = [
+initial_extensions = {
     'cogs.reddit',
     'cogs.raid',
     'cogs.aqual.aqual',
@@ -21,11 +22,14 @@ initial_extensions = [
     'cogs.8ball',
     'cogs.music',
     'cogs.twitch.twitch'
-]
+}
 
+description = """
+Hello! I am a bot written by Globalelite, giving you raid information and more!
+"""
 help_attrs = dict(hidden=True)
 prefix = ['!', '\N{HEAVY EXCLAMATION MARK SYMBOL}']
-bot = commands.Bot(command_prefix=prefix, pm_help=None, help_attrs=help_attrs)
+bot = commands.Bot(command_prefix=prefix, description=description, pm_help=None, help_attrs=help_attrs)
 
 def load_credentials():
     with open('credentials.json') as f:
@@ -39,17 +43,19 @@ if __name__ == '__main__':
     credentials = load_credentials()
 
     token = credentials['BOT_TOKEN']
-    bot.CLIENT_ID = credentials['BOT_CLIENT_ID']
-    bot.CLIENT_SECRET = credentials['BOT_CLIENT_SECRET']
+    bot.CLIENT_ID = credentials['CLIENT_ID']
+    bot.CLIENT_SECRET = credentials['CLIENT_SECRET']
     bot.PRAW_CLIENT_ID = credentials['PRAW']['CLIENT_ID']
     bot.PRAW_CLIENT_SECRET = credentials['PRAW']['CLIENT_SECRET']
     bot.PRAW_USER_AGENT = credentials['PRAW']['USER_AGENT']
-    bot.SHIVTR_HEADERS = credentials['SHIVTR']['HEADERS']
-    bot.SHIVTR_DATA = credentials['SHIVTR']['DATA']
-    bot.SHIVTR_URL = credentials['SHIVTR']['URL']
+    bot.RAID_HEADERS = credentials['RAID']['HEADERS']
+    bot.RAID_DATA = credentials['RAID']['DATA']
+    bot.RAID_URL = credentials['RAID']['URL']
     bot.TRIVIA_ROOM_ID = credentials['TRIVIA_ROOM_ID']
     bot.BLACKJACK_ROOM_ID = credentials['BLACKJACK_ROOM_ID']
     bot.RANDOM_ROOM_ID = credentials['RANDOM_ROOM_ID']
+    bot.TWITCH_CLIENT_ID = credentials['TWITCH_CLIENT_ID']
+    bot.TWITCH_ALERT_CHANNEL = credentials['TWITCH_ALERT_CHANNEL']
 
     bot.remove_command('help')
 
