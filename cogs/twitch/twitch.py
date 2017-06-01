8d0b0238a5a08a63d37b14fa231e9e610831d8eb
Michael West <michaelwest299@gmail.com>
2017-06-01 23:41:10 +0100

    added extra twitch commands

--- cogs/twitch/twitch.py
+++ cogs/twitch/twitch.py
@@ -1,5 +1,6 @@
 from discord.ext import commands
 import discord
+from cogs.utils import checks
 import aiohttp
 import asyncio
 import pickle
@@ -24,11 +25,50 @@ class Twitch:
         with open('cogs/twitch/livestreams.txt', 'wb') as f:
             pickle.dump(livestreams, f)
 
+    @commands.command()
+    @checks.is_owner()
+    async def disablealerts(self):
+        self.task.cancel()
+        await self.bot.say('Twitch alerts have been disabled.')
+
+    @commands.command()
+    @checks.is_owner()
+    async def enablealerts(self):
+        self.task = self.bot.loop.create_task(self.check_streamers())
+        await self.bot.say('Twitch alerts have been enabled.')
+
+    @commands.command()
+    async def removetwitch(self, message: str):
+        livestreams = self.read()
+        if message.lower() in livestreams:
+            livestreams.pop(message, 0)
+            self.write(livestreams)
+            await self.bot.say(message + ' has been removed from the notify list.')
+        else:
+            await self.bot.say(message + ' is not on the notify list.')
+
+    @commands.command()
+    async def addtwitch(self, message: str):
+        livestreams = self.read()
+        if message in livestreams:
+            await self.bot.say(message + ' is already on the notify list.')
+        else:
+            livestreams[message.lower()] = 'offline'
+            self.write(livestreams)
+            await self.bot.say(message + ' has been added to the notify list.')
+
+    @commands.command()
+    async def listtwitch(self):
+        livestreams = self.read()
+        string_list = '\n'.join(livestreams.keys())
+        number = str(len(livestreams.keys()))
+        await self.bot.say(string_list)
+
 
     async def notify_live(self, streamer):
-        url = 'https://api.twitch.tv/kraken/streams/' + streamer + '?client_id=p7k19ow79cnir9695tlo3f800gabrj'
+        url = 'https://api.twitch.tv/kraken/streams/' + streamer + '?client_id=' + self.bot.TWITCH_CLIENT_ID
         async with self.session.get(url) as r:
-            channel = self.bot.get_channel('314866130600853516')
+            channel = self.bot.get_channel(self.bot.TWITCH_ALERT_CHANNEL)
             link = 'https://twitch.tv/' + streamer
             msg = streamer + ' has gone live!'
             js = await r.json()
@@ -57,8 +97,7 @@ class Twitch:
             livestreams = self.read()
             for key in livestreams.keys():
               await self.notify_live(key)
-            await asyncio.sleep(30)
-
+            await asyncio.sleep(45)
 
 def setup(bot):
     bot.add_cog(Twitch(bot))
