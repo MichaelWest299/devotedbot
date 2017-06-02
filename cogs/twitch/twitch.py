205151e56b8c45570f3a0c54cff7a7122fcc21ee
Michael West <michaelwest299@gmail.com>
2017-06-03 00:38:12 +0100

    gave embeds to all commands and fixed naming

--- cogs/twitch/twitch.py
+++ cogs/twitch/twitch.py
@@ -5,8 +5,8 @@ import aiohttp
 import asyncio
 import pickle
 
-class Twitch:
 
+class Twitch:
 
     def __init__(self, bot):
         self.bot = bot
@@ -28,10 +28,11 @@ class Twitch:
     async def disablealerts(self):
         try:
             self.task.cancel()
-            await self.bot.say('Twitch alerts have been disabled.')
+            description = 'Twitch alerts have been disabled.'
         except AttributeError:
-            await self.bot.say('Twitch alerts are already disabled.')
-
+            description = 'Twitch alerts are already disabled.'
+        embed = discord.Embed(description=description, colour=0x8080ff)
+        await self.bot.say(embed=embed)
 
     @commands.command()
     @checks.is_owner()
@@ -39,53 +40,61 @@ class Twitch:
         try:
             self.task.cancel()
             self.task = self.bot.loop.create_task(self.check_streamers())
-            await self.bot.say('Twitch alerts are already enabled.')
+            description = 'Twitch alerts are already enabled.'
         except AttributeError:
             self.task = self.bot.loop.create_task(self.check_streamers())
-            await self.bot.say('Twitch alerts have been enabled.')
-
+            description = 'Twitch alerts have been enabled.'
+        embed = discord.Embed(description=description, colour=0x8080ff)
+        await self.bot.say(embed=embed)
 
     @commands.command()
-    async def removetwitch(self, message: str):
+    async def twitchremove(self, message: str):
         livestreams = self.read()
         if message.lower() in livestreams:
             livestreams.pop(message, 0)
             self.write(livestreams)
-            await self.bot.say(message + ' has been removed from the notify list.')
+            description = message + ' has been removed from the notify list.'
         else:
-            await self.bot.say(message + ' is not on the notify list.')
+            description = message + ' is not on the notify list.'
+        embed = discord.Embed(description=description, colour=0x8080ff)
+        await self.bot.say(embed=embed)
 
     @commands.command()
-    async def addtwitch(self, message: str):
+    async def twitchadd(self, message: str):
         livestreams = self.read()
         if message in livestreams:
-            await self.bot.say(message + ' is already on the notify list.')
+            description = message + ' is already on the notify list.'
         else:
             livestreams[message.lower()] = 'offline'
             self.write(livestreams)
-            await self.bot.say(message + ' has been added to the notify list.')
-
-    @commands.command()
-    async def listtwitch(self):
-        livestreams = self.read()
-        string_list = '\n'.join('[' + item + ']' + '(https://www.twitch.tv/' + item + ')' for item in livestreams.keys())
-        number = str(len(livestreams.keys()))
-        embed = discord.Embed(colour=0x8080ff, title='__Streams__', description=string_list)
+            description = message + ' has been added to the notify list.'
+        embed = discord.Embed(description=description, colour=0x8080ff)
         await self.bot.say(embed=embed)
 
+    @commands.command()
+    async def twitchlist(self):
+        livestreams = self.read()
+        string_list = '\n'.join(
+            '[' + item + ']' + '(https://www.twitch.tv/' + item + ')' for item in livestreams.keys())
+        number = str(len(livestreams.keys()))
+        embed = discord.Embed(
+            colour=0x8080ff, title='__Twitch Notifications__', description=string_list)
+        await self.bot.say(embed=embed)
 
     async def notify_live(self, streamer):
-        url = 'https://api.twitch.tv/kraken/streams/' + streamer + '?client_id=' + self.bot.TWITCH_CLIENT_ID
+        url = 'https://api.twitch.tv/kraken/streams/' + \
+            streamer + '?client_id=' + self.bot.TWITCH_CLIENT_ID
         async with self.session.get(url) as r:
             channel = self.bot.get_channel(self.bot.TWITCH_ALERT_CHANNEL)
             link = 'https://twitch.tv/' + streamer
-            msg = streamer + ' has gone live!'
             js = await r.json()
             livestreams = self.read()
             if js['stream'] and (livestreams[streamer] == 'offline'):
                 livestreams[streamer] = 'online'
                 self.write(livestreams)
-                await self.bot.send_message(channel, msg)
+                description = streamer + ' has gone live!'
+                embed = discord.Embed(description=description, colour=0x8080ff)
+                await self.bot.send_message(channel, embed=embed)
                 await self.bot.send_message(channel, link)
             elif js['stream'] is None and (livestreams[streamer] == 'online'):
                 livestreams[streamer] = 'offline'
@@ -93,16 +102,15 @@ class Twitch:
             else:
                 return
 
-
-
     async def check_streamers(self):
         await self.bot.wait_until_ready()
         await asyncio.sleep(5)
         while not self.bot.is_closed:
             livestreams = self.read()
             for key in livestreams.keys():
-              await self.notify_live(key)
+                await self.notify_live(key)
             await asyncio.sleep(45)
+
 
 def setup(bot):
     bot.add_cog(Twitch(bot))
